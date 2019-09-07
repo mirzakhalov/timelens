3f8a54d56d3079db3389b00efeec1e8879d2c8ff
Jamshidbek Mirzakhalov <mirzakhalov@huntsman-ve702-3788.apng.wlan.private.upenn.edu>
2019-09-07 14:29:40 -0400

    added camera stuff

--- app/src/main/java/com/mirzakhalov/timelens/CameraPreview.java
+++ app/src/main/java/com/mirzakhalov/timelens/CameraPreview.java
@@ -1,78 +1,51 @@
-//package com.mirzakhalov.timelens;
-//
-//import android.graphics.Camera;
-//import android.view.SurfaceHolder;
-//import android.view.SurfaceView;
-//
-//public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
-//    private SurfaceHolder mHolder;
-//    private Camera mCamera;
-//    private String TAG = "CameraPreview";
-//
-//    public CameraPreview(Context context, Camera camera) {
-//        super(context);
-//        mCamera = camera;
-//
-//        // Install a SurfaceHolder.Callback so we get notified when the
-//        // underlying surface is created and destroyed.
-//        mHolder = getHolder();
-//        mHolder.addCallback(this);
-//        // deprecated setting, but required on Android versions prior to 3.0
-//        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-//    }
-//
-//    @Override
-//    public void surfaceCreated(SurfaceHolder holder) {
-//        // The Surface has been created, now tell the camera where to draw the
-//        // preview.
-//        try {
-//            mCamera.setPreviewDisplay(holder);
-//            mCamera.startPreview();
-//        } catch (IOException e) {
-//            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
-//        }
-//    }
-//
-//    @Override
-//    public void surfaceDestroyed(SurfaceHolder holder) {
-//        try {
-//            mCamera.stopPreview();
-//        } catch (Exception e) {
-//            // ignore: tried to stop a non-existent preview
-//        }
-//        mCamera.release();
-//    }
-//
-//    @Override
-//    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-//        // If your preview can change or rotate, take care of those events here.
-//        // Make sure to stop the preview before resizing or reformatting it.
-//
-//        if (mHolder.getSurface() == null) {
-//            // preview surface does not exist
-//            return;
-//        }
-//
-//        // stop preview before making changes
-//        try {
-//            mCamera.stopPreview();
-//        } catch (Exception e) {
-//            // ignore: tried to stop a non-existent preview
-//        }
-//
-//        // set preview size and make any resize, rotate or
-//        // reformatting changes here
-//
-//        // start preview with new settings
-//        StartPreview();
-//    }
-//
-//    public void StartPreview() {
-//        try {
-//            mCamera.setPreviewDisplay(mHolder);
-//            mCamera.startPreview();
-//        } catch (Exception e) {
-//            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
-//        }
-//    }
-//}
+package com.mirzakhalov.timelens;
+
+import android.app.Activity;
+
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
+
+import java.io.IOException;
+
+public class CameraPreview extends Activity implements TextureView.SurfaceTextureListener {
+      private Camera mCamera;
+      private TextureView mTextureView;
+
+      protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+
+          mTextureView = new TextureView(this);
+          mTextureView.setSurfaceTextureListener(this);
+
+          setContentView(mTextureView);
+      }
+
+      public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+          mCamera = Camera.open();
+
+          try {
+              mCamera.setPreviewTexture(surface);
+              mCamera.startPreview();
+          } catch (IOException ioe) {
+              // Something bad happened
+          }
+      }
+
+      public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+          // Ignored, Camera does all the work for us
+      }
+
+      public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+          mCamera.stopPreview();
+          mCamera.release();
+          return true;
+      }
+
+      public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+          // Invoked every time there's a new Camera preview frame
+      }
+  }

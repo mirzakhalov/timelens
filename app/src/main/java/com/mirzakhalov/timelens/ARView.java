c00aab03ef4c963c07cbd8121de7e06d716383e5
Jamshidbek Mirzakhalov <mirzakhalov@huntsman-ve702-3788.apng.wlan.private.upenn.edu>
2019-09-07 02:59:44 -0400

    location working

--- app/src/main/java/com/mirzakhalov/timelens/ARView.java
+++ app/src/main/java/com/mirzakhalov/timelens/ARView.java
@@ -1,13 +1,101 @@
 package com.mirzakhalov.timelens;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ARView extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private double lastLatitude = 0.0;
+    private double lastLongitude = 0.0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // call this function every 10 seconds to get the new location
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (ContextCompat.checkSelfPermission(ARView.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ARView.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            100);
+                } else{
+                    Log.d("Location", "Granted");
+                    getLocation();
+                }
+            }
+        }, 0, 10000);//put here time 1000 milliseconds=1 second
+
+    }
+
+
+    private void getLocation(){
+
+        try{
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            lastLatitude = location.getLatitude();
+                            lastLongitude = location.getLongitude();
+                            Log.d("Location", "Longitude: " + lastLongitude + " Latitude: " + lastLatitude);
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+
+                            }
+                        }
+                    });
+        }
+        catch(SecurityException e){
+            e.fillInStackTrace();
+            Log.d("Location Error", e.toString());
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 100: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    getLocation();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.d("Location permission", "Not granted");
+
+                }
+                return;
+            }
+
+        }
     }
 }

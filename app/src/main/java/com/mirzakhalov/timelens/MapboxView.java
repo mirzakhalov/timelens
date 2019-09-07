d60b10be439c8b04ac369c5acfa1267b6796e174
Jamshidbek Mirzakhalov <mirzakhalov@huntsman-ve702-3788.apng.wlan.private.upenn.edu>
2019-09-07 02:27:26 -0400

    first commit

---
+++ app/src/main/java/com/mirzakhalov/timelens/MapboxView.java
@@ -0,0 +1,13 @@
+package com.mirzakhalov.timelens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapboxView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapbox_view);
+    }
+}
